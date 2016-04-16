5eb2f37b27ad88bd70b6a3148489eaae02ceb440
Tom Anderson <twic@urchin.earth.li>
2016-04-16 16:42:45 +0100

    Provide a script to generate all the fakes; use run it before running the tests

---
+++ scripts/make_fakes.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eu
+
+cd "$(dirname "$0")/.."
+
+# build counterfeiter itself
+counterfeiter='/tmp/counterfeiter_test'
+trap "rm $counterfeiter" EXIT
+go build -o $counterfeiter
+
+# counterfeit all the interfaces we can find
+egrep --recursive --include '*.go' 'type [^ ]* interface {' . \
+  | sed 's#^./\(.*\)/[^/]*.go:type \([^ ]*\) interface {#\1 \2#' \
+  | while read PACKAGE INTERFACE; do $counterfeiter $PACKAGE $INTERFACE; done
+
+# fix an import oddity in the UI fake
+sed -i.bak '/"golang.org\/x\/crypto\/ssh\/terminal"/d' terminal/terminalfakes/fake_ui.go
