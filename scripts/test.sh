bc1602492aa5be5b90befb48cb99709d6e62ec3a
Tim Jarratt <tjarratt@gmail.com>
2017-02-26 20:22:13 -0800

    Add output to the test script
    Mostly because this makes me happier, but also because it helps
    other developers understand what's happening when the tests fail to
    run successfully with the race flag.

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,9 @@ set -eu
 cd "$(dirname "$0")/.."
 
 # counterfeit all the things
+echo
+echo "Generating fakes used by tests..."
+echo
 scripts/make_fakes.sh
 
 # counterfeit through a symlink
@@ -15,10 +18,18 @@ mkdir -p fixtures/symlinked_fixturesfakes
 
 go run main.go -o fixtures/symlinked_fixturesfakes/fake_something.go $symlinked_fixtures Something
 
+sleep 1
+
 # check that the fakes compile
+echo
+echo "Ensuring generated fakes compile..."
+echo
 find ./fixtures/ -type d -name '*fakes' | xargs go build
 
 # run the tests using the fakes
+echo
+echo "Running tests..."
+echo
 go test -race -v ./...
 
 # remove any generated fakes
@@ -28,4 +39,32 @@ go test -race -v ./...
 # and some of our internal interfaces have changed,
 # they will likely have old generated fakes that reference
 # files that no longer exist, breaking their local tests
-find ./fixtures/ -type d -name '*fakes' | xargs rm -rf
+echo
+echo "Removing generated files..."
+echo
+find ./fixtures/ -type d -name '*fakes/fake*.go' | xargs rm -rf
+
+echo "
+ _______  _     _  _______  _______  _______                   
+|       || | _ | ||       ||       ||       |                  
+|  _____|| || || ||    ___||    ___||_     _|                  
+| |_____ |       ||   |___ |   |___   |   |                    
+|_____  ||       ||    ___||    ___|  |   |                    
+ _____| ||   _   ||   |___ |   |___   |   |                    
+|_______||__| |__||_______||_______|  |___|                    
+ _______  __   __  ___   _______  _______                      
+|       ||  | |  ||   | |       ||       |                     
+|  _____||  | |  ||   | |_     _||    ___|                     
+| |_____ |  |_|  ||   |   |   |  |   |___                      
+|_____  ||       ||   |   |   |  |    ___|                     
+ _____| ||       ||   |   |   |  |   |___                      
+|_______||_______||___|   |___|  |_______|                     
+ _______  __   __  _______  _______  _______  _______  _______ 
+|       ||  | |  ||       ||       ||       ||       ||       |
+|  _____||  | |  ||       ||       ||    ___||  _____||  _____|
+| |_____ |  |_|  ||       ||       ||   |___ | |_____ | |_____ 
+|_____  ||       ||      _||      _||    ___||_____  ||_____  |
+ _____| ||       ||     |_ |     |_ |   |___  _____| | _____| |
+|_______||_______||_______||_______||_______||_______||_______|
+"
+
