5a5572d5d2e91432a36764d66fd158ba151d833d
Lars Gohr <larsgohr@gmail.com>
2018-06-23 14:48:55 +0200

    Remove go generate, due to the fact that counterfeiter will support this variant after this commit

--- fixtures/something_remote.go
+++ fixtures/something_remote.go
@@ -4,7 +4,6 @@ import (
 	"github.com/maxbrunsfeld/counterfeiter/fixtures/aliased_package"
 )
 
-//go:generate counterfeiter . SomethingWithForeignInterface
 type SomethingWithForeignInterface interface {
 	the_aliased_package.InAliasedPackage
 }
