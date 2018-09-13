d6dabc6b8726a3cb9891211025dce831052a8109
Joe Fitzgerald <jfitzgerald@pivotal.io>
2018-09-13 11:26:46 -0600

    just use go get ./...

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-
-go list -f '{{ join .Imports "\n"}}{{"\n"}}{{ join .TestImports "\n" }}{{"\n"}}{{ join .XTestImports "\n"}}' ./... \
-    | grep -v "github.com/maxbrunsfeld/counterfeiter" \
-    | grep -E '(github.com)|(golang.org)' \
-    | sort \
-    | uniq \
-    | xargs go get -v
+go get ./...
