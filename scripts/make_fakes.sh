bb98bd063ce2dd8dc32103aa1dba19ee67665a45
Joe Fitzgerald <jfitzgerald@pivotal.io>
2018-09-13 11:19:19 -0600

    ensure counterfeiter is in the path

--- scripts/make_fakes.sh
+++ scripts/make_fakes.sh
@@ -2,12 +2,5 @@
 set -eu
 cd "$(dirname "$0")/.."
 
-# build counterfeiter itself
-counterfeiter='/tmp/counterfeiter_test'
-# shellcheck disable=SC2064
-# we want to use the current value
-trap "rm $counterfeiter" EXIT
-go build -o $counterfeiter
-
-# generate fakes
+go install .
 go generate ./...
