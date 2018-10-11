b2644d713c496d010792e2201a6c08cdaf07f487
Joe Fitzgerald <jfitzgerald@pivotal.io>
2018-10-11 21:50:58 +0200

    add deps script back in

---
+++ scripts/deps.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+go list -f '{{ join .Imports "\n"}}{{"\n"}}{{ join .TestImports "\n" }}{{"\n"}}{{ join .XTestImports "\n"}}' ./... \
+    | grep -v "github.com/maxbrunsfeld/counterfeiter" \
+    | grep -E '(github.com)|(golang.org)' \
+    | sort \
+    | uniq \
+    | xargs go get -v
+go get -u golang.org/x/tools/...
