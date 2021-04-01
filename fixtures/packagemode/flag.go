f2539983b715fd2bc7fd36dd8466c3f7b43d5cf2
Joe Fitzgerald <jfitzgerald@vmware.com>
2021-04-01 13:20:34 -0600

    switch from os to flag for package mode fixtures

--- fixtures/packagemode/flag.go
+++ fixtures/packagemode/flag.go
@@ -1,5 +1,5 @@
 package packagemode
 
 //go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -generate
-//counterfeiter:generate -p os
-//counterfeiter:generate -o oscustomfakesdir -p os
+//counterfeiter:generate -p flag
+//counterfeiter:generate -o flagcustomfakesdir -p flag
